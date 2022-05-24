bf82819671ff9c9b15b76b57910c79119b368ff6
samuel sholaye <samuelsho659@gmail.com>
2022-05-24 09:41:31 -0700

    task_0_edit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,16 @@
  *   * @h: list to print
  *    *
  *     * Return: number of elements in the list
- *      */
+ **/
 size_t print_listint(const listint_t *h)
 {
-		size_t count = 0;
+size_t count = 0;
 
-			while (h != NULL)
-					{
-								printf("%d\n", h->n);
-										h = h->next;
-												count++;
-													}
-				return (count);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
